cdaab7ab3585cad52adb42aab68966fea4e37a65
Jeff Lawlis <jeff.lawlis@yahoo.com>
2017-01-16 17:36:27 -0600

    cleaned up code fixed new item delete bug

--- main.js
+++ main.js
@@ -1,81 +1,54 @@
 var container = $('.container');
-var form = $('form');
-//make new item
-function savePost(form) {
-  var post = {
-    listItem: form.find('.todo-item').val()
-  };
-var li = $('<li class="list-item">' + post.listItem + '</li>');
-var button = $('<button class="delete" type="button">Delete</button>');
-container.append(li, button);
 
-//line-through attempt
-//cross out item
-li.on('click', function(e) {
-  li.css({'text-decoration': 'line-through'});
-});
-li.on('dblclick', function(e) {
-  li.css('text-decoration', 'none');
-});
-
-  var settings = {
-    url: 'http://tiny-za-server.herokuapp.com/collections/todo_list/',
-    type: 'POST',
-    success: function(data, status, xhr) {
-
-
-    },
-    error: function () {
-      console.log('the post didnt work');
-    },
-    contentType: 'application/json',
-    data: JSON.stringify(post)
-  };
-  $.ajax(settings);
-
-}
-
-/////////runtime below:
-//put new item in list
-var settings = {
+function getList(){
+  //retrieving list items
+$.ajax({
   url: 'http://tiny-za-server.herokuapp.com/collections/todo_list/',
   type: 'GET',
-  success: function(data, status, xhr) {
-    data.forEach(function(list, i, arr) {
-      var li = $('<li class="list-item">'+list.listItem+'</li>');
-      var button = $('<button class="delete" type="button">Delete</button>');
-//cross out item
-li.on('click', function(e) {
-  li.css({'text-decoration': 'line-through'});
-});
-li.on('dblclick', function(e) {
-  li.css('text-decoration', 'none');
-});
-
-
-
-
-      button.on('click', function(e) {
-        var settings = {
-          url: 'http://tiny-za-server.herokuapp.com/collections/todo_list/'+list._id,
+  success: function(response){
+    response.forEach(function(item, i, arr){
+      var li = $('<li class="list-item">'+item.description+'</li>');
+      var button = $('<input type="button" id="delete" value="delete"/>');
+      container.append(li);
+      li.append(button);
+      // implementing delete function
+      button.on('click', function(e){
+        $.ajax({
+          url: 'http://tiny-za-server.herokuapp.com/collections/todo_list/'+item._id,
           type: 'DELETE',
-          success: function(data, status, xhr) {
-            console.log('deleted it!');
+          success: function(response){
             li.remove();
           },
-          error: function() {
-            console.log('delete didnt work');
+          error: function(){
+            console.log('error with delete');
           }
-
-        };
-        $.ajax(settings);
+        });
       });
-      container.append(li, button);
     });
-}};
-$.ajax(settings);
-
-form.find('.submit').on('click', function(e) {
-  e.preventDefault();
-  savePost(form);
+  }
 });
+}
+function newListItem(){
+  var item = {
+    description: $('.todo-item').val(),
+  };
+  $.ajax({
+    url: 'http://tiny-za-server.herokuapp.com/collections/todo_list/',
+    type: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(item),
+    success: function(response){
+      getList();
+    }
+  });
+}
+getList();
+function clearInput(){
+  document.querySelector('.todo-item').value = '';
+}
+  $('.submit').on('click', function(e){
+    e.preventDefault();
+    container.empty();
+    newListItem();
+    clearInput();
+  });
